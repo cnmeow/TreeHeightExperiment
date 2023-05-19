464068cd725f3dfadd703d8286853459eb405559
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-05-20 00:40:42 +0700

    Update RedBlackTree.cpp

--- RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree.cpp
@@ -25,167 +25,6 @@ private:
         node->color = 0;
     }
     
-    // Preorder
-    void preOrderHelper(NodePtr node) {
-        if (node != TNULL) {
-            cout << node->data << " ";
-            preOrderHelper(node->left);
-            preOrderHelper(node->right);
-        }
-    }
-    
-    // Inorder
-    void inOrderHelper(NodePtr node) {
-        if (node != TNULL) {
-            inOrderHelper(node->left);
-            cout << node->data << " ";
-            inOrderHelper(node->right);
-        }
-    }
-    
-    // Post order
-    void postOrderHelper(NodePtr node) {
-        if (node != TNULL) {
-            postOrderHelper(node->left);
-            postOrderHelper(node->right);
-            cout << node->data << " ";
-        }
-    }
-    
-    NodePtr searchTreeHelper(NodePtr node, int key) {
-        if (node == TNULL || key == node->data) {
-            return node;
-        }
-        
-        if (key < node->data) {
-            return searchTreeHelper(node->left, key);
-        }
-        return searchTreeHelper(node->right, key);
-    }
-    
-    // For balancing the tree after deletion
-    void deleteFix(NodePtr x) {
-        NodePtr s;
-        while (x != root && x->color == 0) {
-            if (x == x->parent->left) {
-                s = x->parent->right;
-                if (s->color == 1) {
-                    s->color = 0;
-                    x->parent->color = 1;
-                    leftRotate(x->parent);
-                    s = x->parent->right;
-                }
-                
-                if (s->left->color == 0 && s->right->color == 0) {
-                    s->color = 1;
-                    x = x->parent;
-                } else {
-                    if (s->right->color == 0) {
-                        s->left->color = 0;
-                        s->color = 1;
-                        rightRotate(s);
-                        s = x->parent->right;
-                    }
-                    
-                    s->color = x->parent->color;
-                    x->parent->color = 0;
-                    s->right->color = 0;
-                    leftRotate(x->parent);
-                    x = root;
-                }
-            } else {
-                s = x->parent->left;
-                if (s->color == 1) {
-                    s->color = 0;
-                    x->parent->color = 1;
-                    rightRotate(x->parent);
-                    s = x->parent->left;
-                }
-                
-                if (s->right->color == 0 && s->right->color == 0) {
-                    s->color = 1;
-                    x = x->parent;
-                } else {
-                    if (s->left->color == 0) {
-                        s->right->color = 0;
-                        s->color = 1;
-                        leftRotate(s);
-                        s = x->parent->left;
-                    }
-                    
-                    s->color = x->parent->color;
-                    x->parent->color = 0;
-                    s->left->color = 0;
-                    rightRotate(x->parent);
-                    x = root;
-                }
-            }
-        }
-        x->color = 0;
-    }
-    
-    void rbTransplant(NodePtr u, NodePtr v) {
-        if (u->parent == nullptr) {
-            root = v;
-        } else if (u == u->parent->left) {
-            u->parent->left = v;
-        } else {
-            u->parent->right = v;
-        }
-        v->parent = u->parent;
-    }
-    
-    void deleteNodeHelper(NodePtr node, int key) {
-        NodePtr z = TNULL;
-        NodePtr x, y;
-        while (node != TNULL) {
-            if (node->data == key) {
-                z = node;
-            }
-            
-            if (node->data <= key) {
-                node = node->right;
-            } else {
-                node = node->left;
-            }
-        }
-        
-        if (z == TNULL) {
-            cout << "Key not found in the tree" << endl;
-            return;
-        }
-        
-        y = z;
-        int y_original_color = y->color;
-        if (z->left == TNULL) {
-            x = z->right;
-            rbTransplant(z, z->right);
-        } else if (z->right == TNULL) {
-            x = z->left;
-            rbTransplant(z, z->left);
-        } else {
-            y = minimum(z->right);
-            y_original_color = y->color;
-            x = y->right;
-            if (y->parent == z) {
-                x->parent = y;
-            } else {
-                rbTransplant(y, y->right);
-                y->right = z->right;
-                y->right->parent = y;
-            }
-            
-            rbTransplant(z, y);
-            y->left = z->left;
-            y->left->parent = y;
-            y->color = z->color;
-        }
-        delete z;
-        if (y_original_color == 0) {
-            deleteFix(x);
-        }
-    }
-    
     // For balancing the tree after insertion
     void insertFix(NodePtr k) {
         NodePtr u;
@@ -231,24 +70,6 @@ private:
         root->color = 0;
     }
     
-    void printHelper(NodePtr root, string indent, bool last) {
-        if (root != TNULL) {
-            cout << indent;
-            if (last) {
-                cout << "R----";
-                indent += "   ";
-            } else {
-                cout << "L----";
-                indent += "|  ";
-            }
-            
-            string sColor = root->color ? "RED" : "BLACK";
-            cout << root->data << "(" << sColor << ")" << endl;
-            printHelper(root->left, indent, false);
-            printHelper(root->right, indent, true);
-        }
-    }
-    
 public:
     RedBlackTree() {
         TNULL = new Node;
@@ -258,62 +79,6 @@ public:
         root = TNULL;
     }
     
-    void preorder() {
-        preOrderHelper(this->root);
-    }
-    
-    void inorder() {
-        inOrderHelper(this->root);
-    }
-    
-    void postorder() {
-        postOrderHelper(this->root);
-    }
-    
-    NodePtr searchTree(int k) {
-        return searchTreeHelper(this->root, k);
-    }
-    
-    NodePtr minimum(NodePtr node) {
-        while (node->left != TNULL) {
-            node = node->left;
-        }
-        return node;
-    }
-    
-    NodePtr maximum(NodePtr node) {
-        while (node->right != TNULL) {
-            node = node->right;
-        }
-        return node;
-    }
-    
-    NodePtr successor(NodePtr x) {
-        if (x->right != TNULL) {
-            return minimum(x->right);
-        }
-        
-        NodePtr y = x->parent;
-        while (y != TNULL && x == y->right) {
-            x = y;
-            y = y->parent;
-        }
-        return y;
-    }
-    
-    NodePtr predecessor(NodePtr x) {
-        if (x->left != TNULL) {
-            return maximum(x->left);
-        }
-        
-        NodePtr y = x->parent;
-        while (y != TNULL && x == y->left) {
-            x = y;
-            y = y->parent;
-        }
-        
-        return y;
-    }
     
     void leftRotate(NodePtr x) {
         NodePtr y = x->right;
@@ -397,15 +162,6 @@ public:
         return this->root;
     }
     
-    void deleteNode(int data) {
-        deleteNodeHelper(this->root, data);
-    }
-    
-    void printTree() {
-        if (root) {
-            printHelper(this->root, "", true);
-        }
-    }
     int getHeight(NodePtr root) {
         if (root != TNULL) {
             int lHeight = getHeight(root->left),
@@ -415,7 +171,6 @@ public:
         return 0;
     }
 };
-
 int main() {
     ofstream fo("RedBlackHeight.txt"); // Output file
     for (int f = 1; f <= 10; ++f) {
